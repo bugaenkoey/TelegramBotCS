0b3c2811b98f876fc7607a6c2ff30586fbdf56c6
Yevhen <buhaienkoyevhen@gmail.com>
2023-07-31 21:52:37 +0300

    Display of buttons for services and dates

---
+++ Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramBotCS.Models
+{
+    public class Order 
+    {
+        public Order()
+        {
+        }
+
+        public Order(int? serviceId, DateTime? dateTime)
+        {
+            ServiceId = serviceId;
+            DateTime = dateTime;
+        }
+
+        public int Id { get; set; }
+        public Service Service { get; set; }
+        public int? ServiceId { get; set; }
+        public int? UserId { get; set; }
+        public User User { get; set; }
+        public bool Done { get; set; } = false;
+        public DateTime? DateTime { get; set; }
+        public string? Note { get; set; }
+        public string? Comment { get; set; }
+       // public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 using System;
 using System.IO;
 using System.Text;
-
-using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -13,6 +13,11 @@ using Newtonsoft.Json;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using System.Collections.Generic;
+using System.Net.Http;
+using TelegramBotCS.Models;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+using System.Linq;
+using NPOI.SS.Formula.Functions;
 
 namespace TelegramBotCS
 {
@@ -20,6 +25,7 @@ namespace TelegramBotCS
     //  install-package telegram.bot
     class Program
     {
+        public static Order order = new Order();
         static void Main(string[] args)
         {
             //string TELEGRAMBOTTOKEN = GetToken();
@@ -33,12 +39,10 @@ namespace TelegramBotCS
         {
             /*
                 {
-
                  "BOTNAME": "TelegramBot",
                  "TELEGRAMBOTTOKEN": "1234567890:QWERTYUIOP-asdfghjkl-zxcvbnm"
                 }
             */
-            //https://ru.stackoverflow.com/questions/515370/%d0%9a%d0%b0%d0%ba-%d0%bf%d1%80%d0%be%d0%bf%d0%b8%d1%81%d0%b0%d1%82%d1%8c-%d0%bf%d1%83%d1%82%d1%8c-%d0%ba-txt-%d1%84%d0%b0%d0%b9%d0%bb%d1%83/515397#515397
 
             string jsonFilePath = @"..\..\..\Secret\appsettings.json";
             string key = "TELEGRAMBOTTOKEN";
@@ -52,45 +56,211 @@ namespace TelegramBotCS
         async private static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
             var message = update.Message;
+            string callback = string.Empty;
 
             //ReplyKeyboardMarkup keyboardDate = createKeybordDate2(25);
+            Service[] nailService = await GetServiceAsync();
 
+            Order[] orders = await GetTAsync();
+
+            ReplyKeyboardMarkup serviceButtons = CreateServiceButtonsCol(nailService);
+
+            InlineKeyboardMarkup serviceButtonsCallback = CreateServiceColButtonsCallback(nailService);
 
             try
             {
-                if (message != null && message.Text != null)
+                if (update != null && update.CallbackQuery != null)
                 {
-                    //await botClient.SendTextMessageAsync(message.Chat.Id, $"Привіт \nТвій текст:\n{message.Text}");
+                    callback = update.CallbackQuery.Data;
+                    await botClient.SendTextMessageAsync(
+                    update.CallbackQuery.From.Id,
+                    //message.Chat.Id, 
+                    text: $"TEST_Callback {callback}"                
+                    //,replyMarkup: serviceButtonsCallback
+                    );
 
-                    string texto =
-               @"<i> italic </i>, <em> italic </em>" +
-               @"<a href = 'http://www.example.com/'> inline URL </a>" +
-               @"<a href = 'tg://user?id=123456789'> inline mention of a user</a>" +
-               @"<code> inline fixed-width code </code>" +
-               @"<pre> pre - formatted fixed-width code block</pre>";
+                    setOrderServiceId(callback);
 
-                    int.TryParse(message.Text, out var day);
-                    //int day = int.Parse(message.Text);
-            ReplyKeyboardMarkup keyboardDate = createKeybordDate(day);
+                    //ReplyKeyboardMarkup keyboardDate = createKeybordDate(25);
+                    //await botClient.SendTextMessageAsync(
+                    //    chatId: update.CallbackQuery.From.Id,
+                    //    text: "Select Date",
+                    //    parseMode: ParseMode.Html,
+                    //    replyMarkup: keyboardDate);
+
+                    InlineKeyboardMarkup keyboardDate = createKeybordDate2(5);
 
                     await botClient.SendTextMessageAsync(
-                        chatId: message.Chat.Id,
-                        text: texto,
+                        chatId: update.CallbackQuery.From.Id,
+                        text: "Select Date",
                         parseMode: ParseMode.Html,
                         replyMarkup: keyboardDate);
                 }
+            
             }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                if (message != null && message.Text.Equals("/start"))
+                {
+                    await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    //message.Chat.Id, 
+                    text: $"Selecte Service",
+                    parseMode: ParseMode.Html,
+                    replyMarkup: serviceButtonsCallback
+                    );
+                }
+            }
+
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
 
                 await botClient.SendTextMessageAsync(message.Chat.Id, $"Error:\n{ex.Message}");
             }
-            finally
+
+            //try
+            //{
+            //    if (message != null && message.Text != null)
+            //    {
+            //        //await botClient.SendTextMessageAsync(message.Chat.Id, $"Привіт \nТвій текст:\n{message.Text}");
+
+            //        string texto =
+            //   @"<i> italic </i>, <em> italic </em>" +
+            //   @"<a href = 'http://www.example.com/'> inline URL </a>" +
+            //   @"<a href = 'tg://user?id=123456789'> inline mention of a user</a>" +
+            //   @"<code> inline fixed-width code </code>" +
+            //   @"<pre> pre - formatted fixed-width code block</pre>";
+
+            //        int.TryParse(message.Text, out var day);
+            //        //int day = int.Parse(message.Text);
+            //        ReplyKeyboardMarkup keyboardDate = createKeybordDate(day);
+
+            //        await botClient.SendTextMessageAsync(
+            //            chatId: message.Chat.Id,
+            //            text: texto,
+            //            parseMode: ParseMode.Html,
+            //            replyMarkup: serviceButtons);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+
+            //    await botClient.SendTextMessageAsync(message.Chat.Id, $"Error:\n{ex.Message}");
+            //}
+
+
+        }
+
+        private static void setOrderServiceId(string serviceId)
+        {
+            int.TryParse(serviceId, out var id);
+            order.ServiceId = id;
+        }
+        private void setOrderDate(DateTime dateTime)
+        {
+            order.DateTime = dateTime;
+        }
+
+        private Order GetOrder()
+        {
+            return order;
+        }
+
+        private static async Task<Order[]> GetTAsync()
+        {
+
+            string url = @"https://localhost:44331/Order";
+            HttpClient client = new HttpClient();
+            Order[] myObjes = null;
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine("Error");
+                string message = await response.Content.ReadAsStringAsync();
+
+                myObjes = JsonConvert.DeserializeObject<Order[]>(message);
+
             }
 
+            return myObjes;
+        }
+
+        private static InlineKeyboardMarkup CreateServiceColButtonsCallback(Service[] services)
+        {
+        InlineKeyboardButton[][] ik = services.Select(item => new[] {
+                new InlineKeyboardButton(item.Name) { CallbackData = item.Id.ToString() }
+            }).ToArray();
+
+            return new InlineKeyboardMarkup(ik);
+        }
+
+        private static ReplyKeyboardMarkup CreateServiceButtonsCol(Service[] services)
+        {
+            KeyboardButton[][] keyboard2 = services.Select(item => new[] {
+                new KeyboardButton($"{item.Name} {item.Price}грн.")
+
+            }).ToArray();
+
+            //KeyboardButton[][] keyboard = new KeyboardButton[services.Length][];
+            //for (int i = 0; i < services.Length; i++)
+            //{
+            //    var service = new Dictionary<string, int>()
+            //    {
+            //        { $"{services[i].Name} {services[i].Price} грн.", services[i].Id }
+            //    };
+            //    //keyboard[i] = people;
+            //    keyboard[i] = new KeyboardButton[] { $"{services[i].Name} {services[i].Price} грн." };
+            //    //keyboard[i].SetValue(service, index: services[i].Id);
+            //}
+            //ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(keyboard);
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(keyboard2);
+
+            keyboardMarkup.ResizeKeyboard = true;
+            return keyboardMarkup;
+        }
+
+        private static ReplyKeyboardMarkup CreateServiceButtonsRow(Service[] services)
+        {
+            KeyboardButton[][] keyboard = new KeyboardButton[1][];
+            //KeyboardButton[] rov = new KeyboardButton[dayInWeek];
+
+            KeyboardButton[] rov = new KeyboardButton[services.Length];
+            for (int i = 0; i < services.Length; i++)
+            {
+                rov[i] = new KeyboardButton($"{services[i].Name}\n{services[i].Price}");
+            }
+            keyboard[0] = rov;
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(keyboard);
+            keyboardMarkup.ResizeKeyboard = true;
+            return keyboardMarkup;
+        }
+
+        private static async Task<Service[]> GetServiceAsync()
+        {
+            string url = @"https://localhost:44331/Service";
+            HttpClient client = new HttpClient();
+            Service[] product = null;
+
+            //string strProduct = JsonSerializer.Serialize<Service>(product);
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string product2 = await response.Content.ReadAsStringAsync();
+
+                product = JsonConvert.DeserializeObject<Service[]>(product2);
+
+            }
+            return product;
         }
 
         private static ReplyKeyboardMarkup createKeybordDate(int day)
@@ -98,10 +268,10 @@ namespace TelegramBotCS
             int dayWeek = (int)DateTime.Today.DayOfWeek;
             DateTime today = DateTime.Now;
             DateTime dateStrt = DateTime.Now.AddDays(-dayWeek);
-            DateTime dateEnd = DateTime.Now.AddDays(day-1);
+            DateTime dateEnd = DateTime.Now.AddDays(day - 1);
 
             int dayInWeek = 7;
-            int weeks = (int)Math.Ceiling((day+dayWeek) / (double)dayInWeek);
+            int weeks = (int)Math.Ceiling((day + dayWeek) / (double)dayInWeek);
             var keyboard = new KeyboardButton[weeks][];
 
             for (int i = 0; i < weeks; i++)
@@ -109,7 +279,7 @@ namespace TelegramBotCS
                 KeyboardButton[] rov = new KeyboardButton[dayInWeek];
                 for (int j = 0; j < dayInWeek; j++)
                 {
-                    if (dateStrt < today||dateStrt>dateEnd)
+                    if (dateStrt < today || dateStrt > dateEnd)
                     {
                         rov[j] = new KeyboardButton(text: "-");
                     }
@@ -117,13 +287,57 @@ namespace TelegramBotCS
                     {
                         rov[j] = new KeyboardButton("" + dateStrt.Day);
                     }
-                        dateStrt = dateStrt.AddDays(1);
+                    dateStrt = dateStrt.AddDays(1);
                 }
                 keyboard[i] = rov;
             }
 
             ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(keyboard);
             keyboardMarkup.ResizeKeyboard = true;
+            return keyboardMarkup;
+
+        }
+
+
+        private static InlineKeyboardMarkup createKeybordDate2(int day)
+        {
+            int dayWeek = (int)DateTime.Today.DayOfWeek;
+            DateTime today = DateTime.Now;
+            DateTime dateStrt = DateTime.Now.AddDays(-dayWeek);
+            DateTime dateEnd = DateTime.Now.AddDays(day - 1);
+
+            int dayInWeek = 7;
+            int weeks = (int)Math.Ceiling((day + dayWeek) / (double)dayInWeek);
+            var keyboard = new InlineKeyboardButton[weeks][];
+            //+++++++++++++++
+
+            //InlineKeyboardButton[weeks][] ikb = services.Select(item => new[] {
+            //    new InlineKeyboardButton(item.Name) { CallbackData = item.Id.ToString() }
+            //}).ToArray();
+
+            //return new InlineKeyboardMarkup(ikb);
+            //-----------------
+
+            for (int i = 0; i < weeks; i++)
+            {
+                InlineKeyboardButton[] rov = new InlineKeyboardButton[dayInWeek];
+                for (int j = 0; j < dayInWeek; j++)
+                {
+                    if (dateStrt < today || dateStrt > dateEnd)
+                    {
+                        rov[j] = new InlineKeyboardButton(text: "-") { CallbackData = dateStrt.ToString() };
+                    }
+                    else
+                    {
+                        rov[j] = new InlineKeyboardButton("" + dateStrt.Day) { CallbackData = dateStrt.ToString() };
+                    }
+                    dateStrt = dateStrt.AddDays(1);
+                }
+                keyboard[i] = rov;
+            }
+
+            InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup(keyboard);
+            //keyboardMarkup.ResizeKeyboard = true;
             return keyboardMarkup;
 
         }
